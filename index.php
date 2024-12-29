93e2090a674f6c8152e21ccfe9a3287628fb6628
rpardieu <r.pardieu@gmail.com>
2024-12-29 01:27:18 +0100

    passage en release 0.2

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 
 // === Variables à renseigner === //
-$githubZipUrl = "https://github.com/Astik21/devhisto/raw/main/releases/devhisto_v0.1.zip";
+$githubZipUrl = "https://github.com/Astik21/devhisto/raw/main/releases/devhisto_v0.2.zip";
 $rootDir = __DIR__;
 
 // === Fonction pour téléchargement et extraction === //
